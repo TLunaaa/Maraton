9aaa8ee138edc60047307a1156620e28ee511492
nescude <nescude042@hotmail.com>
2018-09-27 22:47:54 -0300

    new corredor2

--- src/Prueba.java
+++ src/Prueba.java
@@ -4,7 +4,7 @@ public class Prueba {
 	public static void main(String[] args) {
 		Maraton maraton = new Maraton();
 		Corredor corredor1 = new Corredor(39435667,"Tomas","Masculino",22,"Junior");
-		Corredor corredor2 = new Corredor(39274042,"Cacho","Masculino",22,"Junior");
+		Corredor corredor2 = new Corredor(39274042,"Nico","Masculino",22,"Junior");
 		maraton.asignarPosicion(corredor1);
 		maraton.asignarPosicion(corredor2);
 	}
