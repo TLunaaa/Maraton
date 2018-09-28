def14faaabb90cded1ff35d957956d9ee0f5da77
TLunaaa <42612882+TLunaaa@users.noreply.github.com>
2018-09-28 00:12:32 -0300

    arreglo asignarPosicion

--- src/Prueba.java
+++ src/Prueba.java
@@ -4,12 +4,12 @@ public class Prueba {
 	public static void main(String[] args) {
 		Maraton maraton = new Maraton();
 
-		Corredor corredor1 = new Corredor(39435667,"Tomas","Masculino",22,"Junior");
+		Corredor corredor1 = new Corredor(39435667,"Tomas","Masculino",22,"");
 		Corredor corredor2 = new Corredor(39274042,"Nico","Masculino",22,"Junior");
 		Corredor corredor3 = new Corredor(39223434,"Tu mama","Masculino",22,"Junior");
-		maraton.asignarPos(corredor1);
-		maraton.asignarPos(corredor2);
-		maraton.asignarPos(corredor3);
+		maraton.asignarPosicion(corredor1);
+		maraton.asignarPosicion(corredor2);
+		maraton.asignarPosicion(corredor3);
 		maraton.revocarPos(corredor2);
 		maraton.listarCorredores();
 		
